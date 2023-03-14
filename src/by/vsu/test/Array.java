c6b175a7c065e3d31734af1f71ad9488ae78fd60
Sergey Yermochenko <yermochenko@gmail.com>
2023-03-14 08:06:04 +0300

    Добавлен класс Array

---
+++ src/by/vsu/test/Array.java
@@ -0,0 +1,36 @@
+package by.vsu.test;
+
+import by.vsu.domain.Product;
+
+public class Array {
+	private Product[] values;
+
+	public Array(int size) {
+		values = new Product[size];
+	}
+
+	public Product get(int index) {
+		return values[index];
+	}
+
+	public void set(int index, Product value) {
+		values[index] = value;
+	}
+
+	public void add(Product value) {
+		Product[] tmp = new Product[values.length + 1];
+		System.arraycopy(values, 0, tmp, 0, values.length);
+		tmp[values.length] = value;
+		values = tmp;
+	}
+
+	private void test() {
+		for(Product value : values) {
+			System.out.println(value);
+		}
+	}
+
+	public int size() {
+		return values.length;
+	}
+}
--- src/by/vsu/test/Main.java
+++ src/by/vsu/test/Main.java
@@ -5,19 +5,30 @@ import by.vsu.domain.Currency;
 import by.vsu.domain.Product;
 
 public class Main {
+	public static void out(Array products) {
+		for(int i = 0; i < products.size(); i++) {
+			System.out.println(products.get(i));
+		}
+	}
 	public static void main(String[] args) {
+		Array products = new Array(3);
+
 		Category category1 = new Category();
 		category1.setName("Молочная продукция");
 		Category category2 = new Category();
 		category2.setName("Мясная продукция");
 
-		Product product1 = new Product("Молоко", new Currency(2L, 87), category1);
-		Product product2 = new Product("Рулька", new Currency(16L, 76));
-		product2.setCategory(category2);
-		Product product3 = new Product("Творог", new Currency(3L, 33), category1);
+		products.set(0, new Product("Молоко", new Currency(2L, 87), category1));
+		products.set(1, new Product("Рулька", new Currency(16L, 76)));
+		products.get(1).setCategory(category2);
+		products.set(2, new Product("Творог", new Currency(3L, 33), category1));
 
-		System.out.println(product1);
-		System.out.println(product2);
-		System.out.println(product3);
+		System.out.println("***********************");
+		out(products);
+
+		products.add(new Product("Салями", new Currency(4L, 74), category2));
+
+		System.out.println("***********************");
+		out(products);
 	}
 }
